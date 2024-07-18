f8484dbe846050fa1f76e8c82db3acfd45756765
Mael Micout <gh api micoutmael3@gmail.com>
2024-07-18 10:11:04 +0200

    done

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,5 +1,12 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
+function maxOfTwoNumbers(a, b) {
+  if(a > b) {
+    return a
+  }
+  else {
+    return b
+  }
+}
 
 
 
@@ -7,7 +14,21 @@ function maxOfTwoNumbers() {}
 // Iteration 2 | Find the Longest Word
 const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if (words.length === 0) {
+    return null; // Retourner 0 si le tableau est vide
+  }
+
+  let maxLength = "";
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLength.length) {
+      maxLength = words[i];
+    }
+  }
+
+  return maxLength; // Retourner la longueur du mot le plus long
+}
 
 
 
@@ -15,7 +36,14 @@ function findLongestWord() {}
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  let sum = 0;
+
+  for (i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i];
+  }
+  return sum
+}
 
 
 
@@ -23,7 +51,22 @@ function sumNumbers() {}
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(numbers2) {
+
+  if (numbers2.length === 0) {
+    return 0;
+  }
+
+  let sum2 = 0;
+
+  for (i = 0; i < numbers2.length; i++) {
+    sum2 = sum2 + numbers2[i];
+  }
+
+  let avg = sum2 / numbers2.length;
+
+  return avg
+}
 
 
 
@@ -31,4 +74,16 @@ function averageNumbers() {}
 // Iteration 5 | Find Elements
 const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
 
-function doesWordExist() {}
+function doesWordExist(words2, checkWord) {
+
+  if (words2.length === 0) {
+    return null;
+  }
+
+  for (let i = 0; i < words2.length; i++) {
+    if (words2[i] === checkWord) {
+      return true;
+    }
+  }
+  return false;
+}
